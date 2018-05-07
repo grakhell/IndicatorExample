e981fb99ce470fabcf4e36a50c867601fc4de542
grakhell <grakhell@gmail.com>
2018-05-07 17:59:49 +0600

    experimental branch

---
+++ recyclerindicator/src/main/java/ru/experimental/indicatorview/Indicator.kt
@@ -0,0 +1,10 @@
+package ru.mininn.recyclerindicator
+
+import android.graphics.Color
+
+class Indicator {
+    private var color:Int = Color.BLACK
+    private var drawable:Int = R.drawable.dot
+    private var size = 10
+    private var margin = 2
+}
